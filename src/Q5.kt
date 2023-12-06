7b6ef1ffd9183cb9178caa634a2eeb4c560f3fee
재영 <jeayeong70@gmail.com>
2023-12-06 09:17:27 +0900

    12/06 알고리즘

---
+++ src/Q5.kt
@@ -0,0 +1,12 @@
+class Q5 {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        if (num1==num2) {
+            answer = 1
+        }
+        else {
+            answer = -1
+        }
+        return answer
+    }
+}
